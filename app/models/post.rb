2bf80dc693341f7a2ca322eac14e6fd5d8c3c46c
Alan Taronna <alan.taronna@gmail.com>
2012-08-30 18:55:10 +0200

    modify post.rb, index.rb, adding a post function

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :name, :title
-
-  validates :name,  :presence => true
-  validates :title, :presence => true,
-                    :lenghth => { :minimum => 8 }
+    attr_accessible :content, :name, :title
+     
+      validates :name,  :presence => true
+      validates :title, :presence => true,
+                        :length => { :minimum => 5 }
 end
